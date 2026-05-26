bb1c6e8646c470d6ce088bf6ba278587155dbc09
Rajveer Singh Jolly <r.jolly@alustudent.com>
2026-05-26 21:51:52 +0200

    fix: use actual template for task 11 with substrings from single str variable

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -2,10 +2,10 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        string str1 = "object-oriented programming in a multilingual puzzle";
-        string str2 = "C# Sharp";
-        Console.WriteLine(str1.Substring(0, 27) + str1.Substring(27, 4) + str2.Substring(0, 2));
-    }
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+
+		Console.WriteLine(str.Substring(49, 15) + str.Substring(79, 12) + str.Substring(118, 3) + str.Substring(101, 3));
+        }
 }
