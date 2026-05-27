b58a686fe95547f19cfde7738d544259181ec8c0
Rajveer Singh Jolly <r.jolly@alustudent.com>
2026-05-27 15:18:53 +0200

    Fix task 0: return int[] not int?[] to match grader main

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,14 +2,14 @@ using System;
 
 class Array
 {
-	public static int?[] CreatePrint(int size)
+	public static int[] CreatePrint(int size)
 	{
 		if (size < 0)
 		{
 			Console.WriteLine("Size cannot be negative");
 			return null;
 		}
-		int?[] array = new int?[size];
+		int[] array = new int[size];
 		for (int i = 0; i < size; i++)
 			array[i] = i;
 		for (int i = 0; i < size; i++)
