f99f691995981d1b9a593edfa4a9fcb2c7847b48
Rajveer Singh Jolly <r.jolly@alustudent.com>
2026-05-27 14:01:06 +0200

    fix: use actual template for task 5 with Main(string[] args)

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        float number = 3.14159F;
-        Console.WriteLine("Float: {0}", number.ToString("F2", System.Globalization.CultureInfo.InvariantCulture));
-    }
+	static void Main(string[] args)
+        {
+		float number = 3.14159f;
+		Console.WriteLine("Float: {0:F2}", number);
+        }
 }
