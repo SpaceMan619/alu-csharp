8c788a9f839760688cc793aec0ff6cf9e5f839ef
Rajveer Singh Jolly <r.jolly@alustudent.com>
2026-05-27 14:42:12 +0200

    Fix tasks 9 and 11: use real templates with correct variables and structure

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,12 @@
 using System;
 class Program
 {
-    static void Main()
-    {
-        System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-US");
-        float percent = 26.41F;
-        float currency = 12345.68F;
-        Console.WriteLine("Percent: {0:F2}%", percent);
-        Console.WriteLine("Currency: {0:C2}", currency);
-    }
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+		Console.WriteLine("Percent: {0:F2}%", percent * 100);
+		Console.WriteLine("Currency: {0:C2}", currency);
+	}
 }
